dc9274828d47ad5ee228b64a7ad3281f584895f7
pyrahdys <42029512+pyrahdys@users.noreply.github.com>
2018-12-06 20:21:32 +0200

    Tehtävä 1.2

--- Viikko 1/tehtava1.1/tehtava1.1/src/index.js
+++ Viikko 1/tehtava1.1/tehtava1.1/src/index.js
@@ -13,10 +13,8 @@ const App = () => {
     return (
         <div>
             <Otsikko kurssi={kurssi} />
-            <Sisalto osa={osa1} tehtavia={tehtavia1} />
-            <Sisalto osa={osa2} tehtavia={tehtavia2} />
-            <Sisalto osa={osa3} tehtavia={tehtavia3} />
-            <Yhteensa yhteensa={tehtavia1 + tehtavia2 + tehtavia3}/>
+            <Sisalto osa1={osa1} tehtavia1={tehtavia1} osa2={osa2} tehtavia2={tehtavia2} osa3={osa3} tehtavia3={tehtavia3} />
+            <Yhteensa tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3}/>
         </div>
     )
 }
@@ -32,7 +30,9 @@ const Otsikko = (props) => {
 const Sisalto = (props) => {
     return (
         <div>
-            <p>{props.osa} {props.tehtavia}</p>
+            <Osa osa={props.osa1} tehtavia={props.tehtavia1}/>
+            <Osa osa={props.osa2} tehtavia={props.tehtavia2}/>
+            <Osa osa={props.osa3} tehtavia={props.tehtavia3}/>
         </div>
     )
 }
@@ -40,7 +40,15 @@ const Sisalto = (props) => {
 const Yhteensa = (props) => {
     return (
         <div>
-            <p>yhteensä {props.yhteensa} tehtävää</p>
+            <p>yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää</p>
+        </div>
+    )
+}
+
+const Osa = (props) => {
+    return (
+        <div>
+            <p>{props.osa} {props.tehtavia}</p>
         </div>
     )
 }

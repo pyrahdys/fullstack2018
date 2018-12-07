b01a3a4db3dcd906fbaed6f88476e9fd93846004
pyrahdys <42029512+pyrahdys@users.noreply.github.com>
2018-12-07 15:14:23 +0200

    Tehtävä 1.6

---
+++ Viikko 1/tehtava1.6/tehtava_1.6/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            hyvä: 0,
+            neutraali: 0,
+            huono: 0
+        }
+    }
+
+    klikHyvä = () => {
+        this.setState({hyvä: this.state.hyvä + 1})
+    }
+
+    klikNeutraali = () => {
+        this.setState({neutraali: this.state.neutraali + 1})
+    }
+
+    klikHuono = () => {
+        this.setState({huono: this.state.huono + 1})
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>anna palautetta</h1>
+                <button onClick={this.klikHyvä}>hyvä</button>
+                <button onClick={this.klikNeutraali}>neutraali</button>
+                <button onClick={this.klikHuono}>huono</button>
+                <h1>statistiikka</h1>
+                <p>hyvä {this.state.hyvä}</p>
+                <p>neutraali {this.state.neutraali}</p>
+                <p>huono {this.state.huono}</p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));

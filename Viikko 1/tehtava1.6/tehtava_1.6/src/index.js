183e809182ec18445a48c63f073c255791f7d50a
pyrahdys <42029512+pyrahdys@users.noreply.github.com>
2018-12-07 19:08:03 +0200

    Tehtävä 1.7

--- Viikko 1/tehtava1.6/tehtava_1.6/src/index.js
+++ Viikko 1/tehtava1.6/tehtava_1.6/src/index.js
@@ -26,16 +26,20 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>anna palautetta</h1>
-                <button onClick={this.klikHyvä}>hyvä</button>
-                <button onClick={this.klikNeutraali}>neutraali</button>
-                <button onClick={this.klikHuono}>huono</button>
+                <div>
+                    <h1>anna palautetta</h1>
+                    <button onClick={this.klikHyvä}>hyvä</button>
+                    <button onClick={this.klikNeutraali}>neutraali</button>
+                    <button onClick={this.klikHuono}>huono</button>
+                </div>
+                <div>
                 <h1>statistiikka</h1>
-                <p>hyvä {this.state.hyvä}</p>
-                <p>neutraali {this.state.neutraali}</p>
-                <p>huono {this.state.huono}</p>
-                <p>keskiarvo {Math.round(((this.state.hyvä - this.state.huono) / (this.state.hyvä + this.state.neutraali + this.state.huono)) * 10) / 10 }</p>
-                <p>positiivisia {Math.round((this.state.hyvä / (this.state.hyvä + this.state.neutraali + this.state.huono)) * 1000) / 10} %</p>
+                    <p>hyvä {this.state.hyvä}</p>
+                    <p>neutraali {this.state.neutraali}</p>
+                    <p>huono {this.state.huono}</p>
+                    <p>keskiarvo {Math.round(((this.state.hyvä - this.state.huono) / (this.state.hyvä + this.state.neutraali + this.state.huono)) * 10) / 10 }</p>
+                    <p>positiivisia {Math.round((this.state.hyvä / (this.state.hyvä + this.state.neutraali + this.state.huono)) * 1000) / 10} %</p>
+                </div>
             </div>
         )
     }
